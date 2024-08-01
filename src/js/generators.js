c773f08323e734c0a20104336aec0b8e11cc5285
Oksana Bannova <oksanchik2170@yandex.ru>
2024-08-01 23:46:31 +0700

    dist

--- src/js/generators.js
+++ src/js/generators.js
@@ -1,4 +1,4 @@
-import Bowman from './js/characters/Bowman';
+import Bowman from './characters/Bowman';
 import Swordsman from './/characters/Swordsman';
 import Magician from './/characters/Magician';
 import Undead from './/characters/Undead';
